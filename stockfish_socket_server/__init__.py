9541063b9f01e847d19299fc8bf2f1b1a1a9d56f
x64squared <108487529+x64squared@users.noreply.github.com>
2022-07-13 11:04:36 -0700

    close ws connection on quit command

--- stockfish_socket_server/__init__.py
+++ stockfish_socket_server/__init__.py
@@ -7,7 +7,6 @@ socket = Sock(app)
 
 @socket.route('/senduci')
 def process_uci(ws):
-    print('User connected!')
     stockfish_socket_emitter = StockfishSocketEmitter(
         stockfish_path=app.config.get('stockfish_path')
     )
@@ -15,7 +14,6 @@ def process_uci(ws):
     while True:
         text = ws.receive()
         if text == 'quit':
-            print('Quit command received, exiting')
             stockfish_socket_emitter.stop()
         else:
             stockfish_socket_emitter.send_uci(text)
